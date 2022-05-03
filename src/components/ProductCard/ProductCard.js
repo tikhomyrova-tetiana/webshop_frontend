a7d4e6303bad0d49940e0f87e3a2e023b22a887e
w.alvesnl <w.alvesnl@gmail.com>
2022-05-03 16:36:54 +0200

    feat/product_card_links_and_style_started

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,17 +1,27 @@
 import React from "react";
 import "./styles.css";
 import StarRating from "../StarRating/StarRating";
+import { NavLink } from "react-router-dom";
+
 export default function ProductCard(props) {
   return (
-    <div>
-      <img
-        className="productImage"
-        src={`${props.imgUrl}`}
-        alt={`${props.title}`}
-      />
-      <p>{props.title}</p>
+    <div className="product-card">
+      <div className="image-container">
+        <NavLink to={`/details/${props.id}`}>
+          <img
+            className="product-image"
+            src={`${props.imgUrl}`}
+            alt={`${props.title}`}
+          />
+        </NavLink>
+        <div className="star-rating">
+          <StarRating stars={props.rating} />
+        </div>
+      </div>
+      <NavLink to={`/details/${props.id}`}>
+        <h3 className="product-title">{props.title}</h3>
+      </NavLink>
       <p>Price: € {props.price}</p>
-      <StarRating stars={props.rating} />
     </div>
   );
 }
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,32 +1,7 @@
-import ProductCard from "../components/ProductCard/ProductCard";
-import axios from "axios";
-import { useEffect, useState } from "react";
-import StarRating from "../components/StarRating/StarRating";
-
 export default function Homepage() {
-  const [getProductList, setProductList] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await axios.get("http://localhost:4000/products");
-      setProductList(response.data);
-    };
-    fetchData();
-  }, []);
-
   return (
     <div>
       <h1>Homepage</h1>
-
-      {getProductList.map((p) => (
-        <ProductCard
-          key={p.id}
-          title={p.title}
-          imgUrl={p.mainImage}
-          rating={p.rating}
-          price={p.price}
-        />
-      ))}
     </div>
   );
 }

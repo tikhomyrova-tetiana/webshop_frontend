f839317d55d12c8a8bcc06ae2db27e8ed92f1d8f
tikhomyrova-tetiana <tihomirova.tania@gmail.com>
2022-05-03 16:30:38 +0200

    prod_detail_description_popup_added

---
+++ src/components/ProductDetails/ProductDetails.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import "./styles.css"
+import axios from 'axios'
+import {useState, useEffect} from 'react'
+import {BsFacebook, BsInstagram, BsTwitter} from 'react-icons/bs'
+import Popup from '../Popup/Popup'
+
+function ProductDetail (props) {
+    const {id} = props
+    
+    const [productDetail, setProductDetail] = useState(null)
+    const [isOpen, setIsOpen] = useState(false);
+ 
+    const togglePopup = () => {
+        setIsOpen(!isOpen);
+    }
+
+    useEffect(() => {
+        const details = async () => {
+            const detailsResponse = await axios.get(`http://localhost:4000/products/${id}`)
+            setProductDetail(detailsResponse.data)
+        }
+        details()
+    }, [])
+
+    return productDetail ? (
+        <div className="detailView">
+           <div>
+            <img className="productDetailImg" src={productDetail.mainImage} alt="product"/>
+           </div>
+           <div className="details">
+            <h1>{productDetail.title}</h1>
+            <div className='twoElements'>
+                <h2>{productDetail.rating}</h2>
+                <div>
+                <button onClick={togglePopup}>Add review</button>
+                {isOpen && <Popup
+                content={<>
+                    <h1>Add review</h1>
+                    <h6>Name</h6>
+                    <input></input>
+                    <h6>Rating</h6>
+                    <h6>Review</h6>
+                    <input></input>
+                </>}
+                handleClose={togglePopup}
+                />}
+                </div>
+            </div>
+            <h2>{productDetail.price}</h2>
+            <p>{productDetail.description}</p>
+            <div className="twoElements">
+                <button>Add to Cart</button>
+                <button>Favorite</button>
+            </div>
+            <h4>Categories: </h4>
+            <div className="shareIcons">Share  <BsFacebook /><BsInstagram /><BsTwitter /></div>
+           </div>
+        </div>
+    ) : (
+        <p>Wait..</p>
+    )
+
+}
+
+export default ProductDetail
--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,7 +1,22 @@
-export default function Details() {
+ import React from 'react'
+ import axios from 'axios'
+ import {useState, useEffect} from 'react'
+ import {useParams} from 'react-router-dom'
+ import ProductDetail from '../components/ProductDetails/ProductDetails'
+ import ProductDescription from '../components/ProductDescription/ProductDescription'
+
+ 
+function DetailsPage() {
+
+    const {id} = useParams()
+
+
+
     return (
         <div>
-            <h1>Details</h1>
-        </div>
-    )
+           <ProductDetail id={id}/>
+           <ProductDescription id={id}/>
+        </div>)
 }
+
+export default DetailsPage

1916fddb3baa42995083174beae66314795b6fff
w.alvesnl <w.alvesnl@gmail.com>
2022-05-03 13:34:18 +0200

    feat/started_products_card

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import './App.css';
-import {Route, Routes} from "react-router-dom";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Shop from "./pages/Shop";
 import Details from "./pages/Details";
 import { NavBar } from "../src/components/NavBar/NavBar";
-
+import ProductCard from "./components/ProductCard/ProductCard";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-     <Routes>
-       <Route path="/" element={<Homepage/>} />
-       <Route path="/shop" element={<Shop/>} />
-       <Route path="/details/:id" element={<Details/>} />
-     </Routes>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/details/:id" element={<Details />} />
+      </Routes>
     </div>
   );
 }
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,7 +1,13 @@
+import ProductCard from "../components/ProductCard/ProductCard";
+//import productData from "../../products.json";
+
+//const productsList = [...productData];
+
 export default function Homepage() {
-    return (
-        <div>
-            <h1>Homepage</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Homepage</h1>
+      <ProductCard />
+    </div>
+  );
 }

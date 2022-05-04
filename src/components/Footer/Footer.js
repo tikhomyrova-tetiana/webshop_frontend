c16a6cb3dec812b3bfb2b080d3bf2a494e3cb063
tikhomyrova-tetiana <tihomirova.tania@gmail.com>
2022-05-04 16:33:01 +0200

    feat/footer-css

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,7 @@
 import "./styles.css"
 import React from "react";
 import { NavLink, Link } from "react-router-dom";
+import { BsFacebook, BsInstagram, BsTwitter } from "react-icons/bs"
 
 
 const Footer = (props) => {
@@ -8,7 +9,7 @@ const Footer = (props) => {
     const {id} = props
     return (
         <div className="footer">
-            <div><NavLink to={"/"} ><span style={{color: "white"}}>OUR </span><span style={{color: "#8568FF"}}>SHOP</span></NavLink></div>
+            <div className="footer-logo"><NavLink to={"/"} ><span style={{color: "white"}}>OUR </span><span style={{color: "#8568FF"}}>SHOP</span></NavLink></div>
             <div className="lists">
                 <div className="footer-list">
                     <h3>Categories</h3>
@@ -26,9 +27,10 @@ const Footer = (props) => {
                     <div><Link to={`/`} >Orders History</Link></div>
                     <div><Link to={`/`} >Order Tracking</Link></div>
                 </div>
-                <div className="footer-list">
+            </div>
+            <div className="footer-list-follow">
                     <h3>Follow us</h3>
-                </div>
+                    <div className="footer-icons"><BsFacebook style={{color:"white"}}/><BsInstagram /><BsTwitter /></div>
             </div>
         </div>
     )

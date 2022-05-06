1cfe06893f481bcce8ee84d60ada89d074c50db9
w.alvesnl <w.alvesnl@gmail.com>
2022-05-06 13:09:34 +0200

    fixed_homepage_text

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -1,21 +1,18 @@
-import React from 'react'
- import axios from 'axios'
- import {useState, useEffect} from 'react'
- import {useParams} from 'react-router-dom'
-import Banner from '../components/Banner/Banner'
- 
- 
+import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import Banner from "../components/Banner/Banner";
+
 function CategoryPage() {
+  // const {id} = useParams()
 
-    // const {id} = useParams()
-
-
-
-    return (
-        <div>
-            <Banner />
-           <h1>Categoty</h1>
-        </div>)
+  return (
+    <div>
+      <Banner />
+      <h1>Category</h1>
+    </div>
+  );
 }
 
-export default CategoryPage
+export default CategoryPage;
--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -2,62 +2,61 @@ import { NavLink } from "react-router-dom";
 import Banner from "../../components/Banner/Banner";
 import "./styles.css";
 
-
 export default function Homepage() {
   return (
     <div>
       <Banner />
       <div className="homepage">
-      <div className="shop-image">
-        <NavLink to="/shop">
-          <img
-            src="https://i.ibb.co/CMj9tdj/shop.jpg"
-            alt="shop"
-            width="100%"
-            height="100%"
-          />
-        </NavLink>
-        <div>SHOP</div>
+        <div className="shop-image">
+          <NavLink to="/shop">
+            <img
+              src="https://i.ibb.co/CMj9tdj/shop.jpg"
+              alt="shop"
+              width="100%"
+              height="100%"
+            />
+          </NavLink>
+          <div>SHOP</div>
+        </div>
+        <div className="four-categories">
+          <div className="small-image">
+            <img
+              src="https://i.ibb.co/XzNMScs/women-fashion.jpg"
+              alt="woman"
+              width="100%"
+              height="100%"
+            />
+            <div>WOMEN'S FASHION</div>
+          </div>
+          <div className="small-image">
+            <img
+              src="https://i.ibb.co/6NSvkvj/mens-fashion.jpg"
+              alt="mens fashion"
+              width="100%"
+              height="100%"
+            />
+            <div>MEN'S FASHION</div>
+          </div>
+          <div className="small-image">
+            <img
+              src="https://i.ibb.co/jGs7c1c/jewelry.jpg"
+              alt="jewelry"
+              width="100%"
+              height="100%"
+            />
+            <div>JEWELRY</div>
+          </div>
+          <div className="small-image">
+            <img
+              src="https://i.ibb.co/pd5nJ6m/electronics.jpg"
+              alt="electronics"
+              width="100%"
+              height="100%"
+            />
+            <div>ELECTRONICS</div>
+          </div>
+        </div>
       </div>
-      <div className="four-categories">
-        <div className="small-image">
-          <img
-            src="https://i.ibb.co/XzNMScs/women-fashion.jpg"
-            alt="woman"
-            width="100%"
-            height="100%"
-          />
-          <div>WOMEN'S APPAREL</div>
-        </div>
-        <div className="small-image">
-          <img
-            src="https://i.ibb.co/6NSvkvj/mens-fashion.jpg"
-            alt="mens fashion"
-            width="100%"
-            height="100%"
-          />
-          <div>MEN'S APPAREL</div>
-        </div>
-        <div className="small-image">
-          <img
-            src="https://i.ibb.co/jGs7c1c/jewelry.jpg"
-            alt="jewelry"
-            width="100%"
-            height="100%"
-          />
-          <div>JEWELRY</div>
-        </div>
-        <div className="small-image">
-          <img
-            src="https://i.ibb.co/pd5nJ6m/electronics.jpg"
-            alt="electronics"
-            width="100%"
-            height="100%"
-          />
-          <div>ELECTRONICS</div>
-        </div>
-      </div>
-    </div>
     </div>
   );
 }
